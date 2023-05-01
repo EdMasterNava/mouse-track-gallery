d3bf8448524f4a2b2a17783d0513f2fde01d8db5
EdMasterNava <eduardonava2ms@gmail.com>
2023-05-01 01:51:10 -0700

    Added Gallery

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Typography from './modules/components/Typography';
+import { Box, Paper, Typography } from '@mui/material';
 
 import style from './modules/styles/styles';
 import withRoot from './modules/styles/withRoot';
@@ -7,11 +7,48 @@ import withRoot from './modules/styles/withRoot';
 
 function Home() {
     const css = style();
+    const gallery = document.getElementById("gallery");
+    const [mouseData, setMouseData] = React.useState({
+        x: null,
+        y: null,
+        xPan: null,
+        yPan: null
+    })
+    React.useEffect(() => {
+        const xDecimal = mouseData.x / window.innerWidth;
+        const yDecimal = mouseData.y / window.innerHeight;
+
+        const xMax = gallery.offsetWidth - window.innerWidth;
+        const yMax = gallery.offsetHeight - window.innerHeight;
+
+        setMouseData(prev => {
+            return {...prev, xPan: xMax * xDecimal * -1, yPan: yMax * yDecimal * -1}
+        });
+    }, [mouseData, gallery]);
+
+    const handleMouse = (event) => {
+        setMouseData(prev => {
+            return {...prev, x: event.pageX, y: event.pageY}
+        });
+    }
     return (
         <>
-            <Typography variant="h1" sx={{textAlign: 'center'}}>
-                Hello World
-            </Typography>
+            <Box sx={{...css.title}}>
+                <Typography variant='h1' color='whitesmoke'>
+                    gallery
+                </Typography>
+            </Box>
+            <Box id="gallery" onMouseMove={handleMouse} sx={{...css.gallary, transform: `translate(${mouseData.xPan}px, ${mouseData.yPan}px)`}}>
+                <Box component={Paper} sx={{...css.tile, ...css.tile1}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile2}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile3}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile4}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile5}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile6}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile7}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile8}}/>
+                <Box component={Paper} sx={{...css.tile, ...css.tile9}}/>
+            </Box>
         </>
     );
 }
--- src/pages/modules/styles/styles.js
+++ src/pages/modules/styles/styles.js
@@ -1,5 +1,87 @@
 const style = () => ({
     //Custom Styles 
+    title: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        position: 'fixed',
+        display: 'flex',
+        width: '100%',
+        zIndex: 0
+    },
+    gallary: {
+        position: 'absolute',
+        height: '140vmax',
+        width: '140vmax'
+    },
+    tile: {
+        borderRadius: '1vmax',
+        transition: 'transform 400ms ease',
+        position: 'absolute'
+    },
+    tile1: {
+        backgroundColor: '#8f2d56',
+        height: '14%',
+        width: '20%',
+        left: '5%',
+        top: '5%',
+    },
+    tile2: {
+        backgroundColor: '#34623f',
+        height: '24%',
+        width: '14%',
+        left: '42%',
+        top: '12%',
+    },
+    tile3: {
+        backgroundColor: '#f0a7a0',
+        height: '18%',
+        width: '16%',
+        left: '12%',
+        top: '34%',
+    },
+    tile4: {
+        backgroundColor: '#efcb68',
+        height: '14%',
+        width: '12%',
+        left: '45%',
+        top: '48%',
+    },
+    tile5: {
+        backgroundColor: '#04395e',
+        height: '16%',
+        width: '32%',
+        left: '8%',
+        top: '70%',
+    },
+    tile6: {
+        backgroundColor: '#6ba292',
+        height: '24%',
+        width: '24%',
+        left: '68%',
+        top: '8%',
+    },
+    tile7: {
+        backgroundColor: '#ce96a6',
+        height: '16%',
+        width: '20%',
+        left: '50%',
+        top: '74%',
+    },
+    tile8: {
+        backgroundColor: '#720026',
+        height: '24%',
+        width: '18%',
+        left: '72%',
+        top: '42%',
+    },
+    tile9: {
+        backgroundColor: '#ffd166',
+        height: '10%',
+        width: '8%',
+        left: '84%',
+        top: '84%',
+    },
 });
 
 export default style;
